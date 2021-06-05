d937a1c25f3213397ebb2e9cca2a329755de4aaa
Jessica Robo <77417832+jessicarobo@users.noreply.github.com>
2021-06-05 06:43:36 -0500

    Update focal.sh

--- installers/focal.sh
+++ installers/focal.sh
@@ -9,18 +9,20 @@ if [[ ! $(which apt) ]]; then
 	echo "This script is intended for 64bit Debian/Ubuntu and requires APT, but couldn't find it." 
 	exit 90
 fi
-echo "Using 'focal.sh' to run APT and dpkg commands..."
+echo -n "Using 'focal.sh' to run APT and dpkg commands... "
 apt update -y &> /dev/null
 apt install -y $dependencies &> /dev/null
+echo -e "$GOK"
 # get caddy 
 if [[ $getCaddy ]]; then
+	echo -n "Getting Caddy webserver... "
 	wget https://github.com/caddyserver/caddy/releases/download/v${CADDYVERSION}/caddy_${CADDYVERSION}_linux_amd64.deb &> /dev/null
 	dpkg -i caddy_${CADDYVERSION}_linux_amd64.deb &> /dev/null
 	rm caddy_${CADDYVERSION}_linux_amd64.deb &> /dev/null
+	echo -e "$GOK"
 fi
 if [[ ! $(which java) ]]; then 
 	echo "Failed to install dependencies (Java not found)"
 	exit 106
 fi
-echo -e "$GOK"
 exit 0
