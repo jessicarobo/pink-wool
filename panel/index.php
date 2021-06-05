3320032ad8018ff6d2b92d67de7d817caf3a2c1a
Jessica Robo <77417832+jessicarobo@users.noreply.github.com>
2021-06-05 05:25:01 -0500

    Update index.php

--- panel/index.php
+++ panel/index.php
@@ -1,9 +1,9 @@
 <?php include("/var/opt/minecraft/www/header.php"); ?>
 	<title>Pink Wool Minecraft Server</title>
 </head>
-<body>
+<body><div id="helpr">
   <h1>hiiii~</h1>
   <p>Welcome to your Pink Wool minecraft server page! This page is located under /var/opt/minecraft/www/ </p>
-  <p><a href="admin/index.php">Admin panel</a></p>
+  <p><a href="admin/index.php">Admin panel</a></p></div>
 <?php include("/var/opt/minecraft/www/footer.php");
 ?>
