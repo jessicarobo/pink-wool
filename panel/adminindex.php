a4a733ff468b5af4d24be9f2e0b3ab79a6d84ca2
Jessica Robo <77417832+jessicarobo@users.noreply.github.com>
2021-06-07 05:23:45 -0500

    Update adminindex.php

--- panel/adminindex.php
+++ panel/adminindex.php
@@ -4,15 +4,28 @@
 	<title>Pink Wool Admin Panel</title>
 </head>
 <body><div id="helpr">
-  <h1>Pink Wool Admin Panel</h1>
+	<h1>Pink Wool Admin Panel</h1>
 		<?php
 			exec("sudo /usr/bin/pink-wool status",$out,$err);
 			echo '<h2>Backend status:</h2><ol id="status">';
 			foreach ($out as $o) {
 				echo "<li>$o</li>";
 			}
-			echo("</ol> <hr><div class='gridwrap'><div class='grid1'><h2>Backups</h2>");
-			$zipfiles = glob('/var/opt/minecraft/www/admin/backups/*.zip');
+		?>
+		</ol> <hr>
+		<h2>Console</h2>
+		<div id='consoleout'>
+			<em>Loading Minecraft console...</em>
+		</div>
+		<form id="sendCommand" method="post" action="pwexec.php">
+			<input id='pwexec' name='pwexec' size=120>
+			<input type="submit" value="Send">
+		</form>
+		<hr>
+		<div class='gridwrap'><div class='grid1'>
+		<h2>Backups</h2>
+		<?php
+		$zipfiles = glob('/var/opt/minecraft/www/admin/backups/*.zip');
 			if (isset($zipfiles)) {
 				echo '<ul>';
 				foreach ($zipfiles as $zip) {
@@ -28,5 +41,6 @@
 		<p><a href="service.php?do=stop">Stop Minecraft</a></p>
 		<p><a href="service.php?do=restart">Restart Minecraft (stop then start)</a></p>
 		<br></div></div></div>
+<script src="/pink-wool.js"></script>
 <?php include("/var/opt/minecraft/www/footer.php");
 ?>
